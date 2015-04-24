b4edfc7b2fb701729eac003c3ded0f5b3fbd99c2
colinbrown91 <colin@Colins-MacBook-Air.local>
2015-04-24 13:36:35 -0500

    test

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,15 +4,15 @@
 <h2>All Products</h2>
 	<!-- blade way to iterate through todo_lists -->
 	@foreach ($products as $product)
-		<h4>{!! link_to_route('products.show', $product->product_name, [$product->id] ) !!}</h4>
+		<h4>{{ link_to_route('products.show', $product->product_name, [$product->id] ) }}</h4>
 		<ul class = "no-bullet button-group">
 			<li>
-				{!! link_to_route('products.edit' , 'Edit' , [$product->id] , ['class'=>'tiny button'])!!}
+				{{ link_to_route('products.edit' , 'Edit' , [$product->id] , ['class'=>'tiny button'])}}
 			</li>
 			<li>
-				{!! Form::model($product, ['route'=> ['products.destroy', $product->id], 'method' => 'delete']) !!}
-					{!! Form::button('Delete', ['type'=>'submit', 'class'=>'tiny alert button'])!!}
-				{!! Form::close() !!}
+				{{ Form::model($product, ['route'=> ['products.destroy', $product->id], 'method' => 'delete']) }}
+					{{ Form::button('Delete', ['type'=>'submit', 'class'=>'tiny alert button'])}}
+				{{ Form::close() }}
 			</li>
 				
 		</ul>
