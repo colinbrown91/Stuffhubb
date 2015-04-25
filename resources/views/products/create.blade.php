165d8e6fdda538cdb8dd884e4249f0b0cfa49308
mvdubois <mvdubois@gmail.com>
2015-04-25 13:54:52 -0500

    Partial
    Partial is _form not __form

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -3,7 +3,7 @@
 
 	{!! Form::open(array('route' => 'products.store')) !!}
 		<div class='form-group'>
-			@include('products.partials.__form')
+			@include('products.partials._form')
 		</div>
 	{!! Form::close() !!}
 
