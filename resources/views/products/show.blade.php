ab01d5466dc7c5ce7a28b738bb1b02d4c3567e08
mvdubois <mvdubois@gmail.com>
2015-04-25 16:34:29 -0500

    04252015 5
    show

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -4,7 +4,7 @@
 	<div>
 		<h2> {!! $product->product_name !!} </h2>
 		<h2> {!! $product->price !!} </h2>
-		<p> {!! link_to_route('todos.index', 'back', null, ['class' => 'small button']) !!} </p>
+		<p> {!! link_to_route('products.index', 'back', null, ['class' => 'small button']) !!} </p>
 	</div>
 
 @endsection
