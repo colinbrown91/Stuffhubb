f1a9e806abe64d74f6f738cef0a9dd8f4d3fd1bf
mvdubois <mvdubois@gmail.com>
2015-04-25 14:09:42 -0500

    Product Controller
    renamed the var to match below

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -49,7 +49,7 @@ class ProductController extends Controller {
 	{
 		// Know how to get single input
 		// shown below getting name of product
-		$name = Input::get('product_name');
+		$product_name = Input::get('name');
 		// How do we get two inputs?
 		// Name
 		// Price
