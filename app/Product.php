e1ffba580fb16d5bba741427bd73ad6b6d0e1534
mvdubois <mdubois@stuffhubb.com>
2015-06-24 15:43:09 -0500

    commenting
    commented Product and ProductPhoto models

--- app/Product.php
+++ app/Product.php
@@ -1,19 +1,36 @@
-<?php namespace App;
+<?php namespace App; 
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model; 
 
 class Product extends Model {
 
+	/**
+     * The user function of this model.
+     * Products belong to users.
+     * @var this - user that the product belongs to.
+     *
+	 */
 	public function user()
 	{
 		return $this->belongsTo('App\User');
 	}
 
+	/**
+     * The productPhotos function of this model.
+     * productPhotos belong to products.
+     * @var this - array of photos that belong to the product.
+     *
+     */
 	public function productPhotos()
 	{
 		return $this->hasMany('App\ProductPhoto');
 	}
 
+    /**
+     * The delete function of this model.
+     * Capture all children of the parent when deleting.
+     * 
+     */
 	public function delete()
 	{
 		ProductPhoto::where('product_id', $this->id)->delete();
--- app/ProductPhoto.php
+++ app/ProductPhoto.php
@@ -4,6 +4,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductPhoto extends Model {
 
+	/**
+     * The Product function of this model.
+     * Photos belong to products.
+     * @var this - product that the photo belongs to.
+     *
+	 */
 	public function Product()
 	{
 		return $this->belongsTo('App\Product');
