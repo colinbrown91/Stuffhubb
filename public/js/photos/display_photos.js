993b3e948b4a3d0928b2de696815f41842eccd9d
mvdubois <mdubois@stuffhubb.com>
2015-06-26 20:06:01 -0500

    photo preview
    preview photos before upload

--- public/js/photos/display_photos.js
+++ public/js/photos/display_photos.js
@@ -1,11 +1,16 @@
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
+function displayPhotos() {
+// function readURL(input) {
+    // if (input.files && input.files[0]) {
+    //     var reader = new FileReader();
 
-        reader.onload = function (e) {
-            $('#screenshot').attr('src', e.target.result);
-        };
+    //     reader.onload = function (e) {
+    //         $('#screenshot').attr('src', e.target.result);
+    //     };
 
-        reader.readAsDataURL(input.files[0]);
-    }
+    //     reader.readAsDataURL(input.files[0]);
+    // }
+
+	var id = document.getElementById("photoDisplay");
+    var name = document.forms["photoUpload"]["file_0"].value;
+    return id.innerHTML=name;
 }
--- resources/views/photos/create.blade.php
+++ resources/views/photos/create.blade.php
@@ -4,11 +4,15 @@
 	<h2> {!! $product->product_name !!} </h2>
 	<h2> {!! $product->price !!} </h2>
 
-	{!! Form::open(array('route' => ['products.photos.store', $product->id], 'files' => 'true', 'enctype' => 'multipart/form-data')) !!}
+	{!! Form::open(array('route' => ['products.photos.store', $product->id], 'name' => 'photoUpload', 'files' => 'true', 'enctype' => 'multipart/form-data', 'onchange' => 'displayPhotos()')) !!}
 		<div class='form-group'>
 			@include('photos.partials._form')
 		</div>
 	{!! Form::close() !!}
+
+	<div id="photoDisplay">
+
+	</div>
 	
 	{!! Html::script('js/photos/display_photos.js') !!}
 
