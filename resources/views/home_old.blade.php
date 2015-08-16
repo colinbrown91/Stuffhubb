8b87349f7b27d100255c7acea9f4812fe59e15ed
colinbrown91 <colin@Colins-MacBook-Air.local>
2015-08-16 18:18:56 -0500

    test colin

--- resources/views/home_old.blade.php
+++ resources/views/home_old.blade.php
@@ -5,7 +5,7 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Home 2</div>
+				<div class="panel-heading">Home Colin</div>
 
 				<div class="panel-body">
 					{{-- {!! $user = Auth::user()->name !!} --}}
